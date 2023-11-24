fb0ecefea944b8bd70b111780b49677ba7b4e030
Riven8192 <jgo@kataf.nl>
2023-11-24 12:10:25 +0100

    Release 0.1.4 - Improve mass calculations, add customizable mod-settings

--- TrainSpeeds/control.lua
+++ TrainSpeeds/control.lua
@@ -1,5 +1,26 @@
+local GAME_FRAMERATE = 60.0;
+
+function math_sign(x)
+   if x < 0 then
+     return -1
+   elseif x > 0 then
+     return 1
+   else
+     return 0
+   end
+end
+
+function math_pow2(x)
+	return x * x
+end
+
+
 function getTrainSpeed(train)
-	return train.speed * 60.0 * 3.6;
+	return train.speed * GAME_FRAMERATE * 3.6;
+end
+
+function setTrainSpeed(train, speed)
+	train.speed = speed / GAME_FRAMERATE / 3.6;
 end
 
 
@@ -27,7 +48,7 @@ end
 
 
 function getCargoWagonUsageRatio(train)
-	local wagon_stack_count = 40;	
+	local wagon_stack_count = game.entity_prototypes['cargo-wagon'].get_inventory_size(defines.inventory.cargo_wagon);
 	local train_stack_used = 0.0;
 	for itemName, amount in pairs(train.get_contents()) do
 		train_stack_used = train_stack_used + amount / game.item_prototypes[itemName].stack_size;
@@ -48,7 +69,7 @@ end
 
 
 function getFluidWagonUsageRatio(train)
-	local wagon_stack_count = 25000;	
+	local wagon_stack_count = game.entity_prototypes['fluid-wagon'].fluid_capacity;
 	local train_stack_used = 0.0;
 	for itemName, amount in pairs(train.get_fluid_contents()) do
 		train_stack_used = train_stack_used + amount;
@@ -63,14 +84,11 @@ function findTrains()
 	for _idx1_, surface in pairs(game.surfaces) do
 		for _idx2_, train in pairs(surface.get_trains()) do
 			global.modtrainspeeds.trainId2train[train.id] = train;
-			global.modtrainspeeds.trainId2mass[train.id] = getTrainMass(train);
+			global.modtrainspeeds.trainId2mass[train.id]  = getTrainMass(train);
 			global.modtrainspeeds.trainId2force[train.id] = getTrainForce(train);
 		end
 	end
 end
-
-
-
 
 
 
@@ -82,6 +100,11 @@ function ensure_mod_context()
 		global.modtrainspeeds.trainId2force = {}
 		global.modtrainspeeds.trainId2speed = {}
 	end
+	
+	if not global.modtrainspeeds.rndm then
+		global.modtrainspeeds.rndm = game.create_random_generator()
+		global.modtrainspeeds.rndm.re_seed(1337);
+	end
 end
 
 
@@ -89,13 +112,13 @@ end
 function getTrainMass(train)
 	local total = 0.0;
 	
-	total = total + getLocomotiveCount(train) * 12500.0;
+	total = total + getLocomotiveCount(train)      * settings.global["modtrainspeeds-locomotive-weight"].value;
 	
-	total = total + getCargoWagonCount(train) * 10000.0;
-	total = total + getCargoWagonUsageRatio(train) * 10000.0;
+	total = total + getCargoWagonCount(train)      * settings.global["modtrainspeeds-cargo-wagon-weight"].value;
+	total = total + getCargoWagonUsageRatio(train) * settings.global["modtrainspeeds-cargo-payload-weight"].value;
 	
-	total = total + getFluidWagonCount(train) * 5000.0;
-	total = total + getFluidWagonUsageRatio(train) * 15000.0;
+	total = total + getFluidWagonCount(train)      * settings.global["modtrainspeeds-fluid-wagon-weight"].value;
+	total = total + getFluidWagonUsageRatio(train) * settings.global["modtrainspeeds-fluid-payload-weight"].value;
 	
 	return total;
 end
@@ -103,20 +126,17 @@ end
 
 
 function getTrainForce(train)
-	local trainSpeed = getTrainSpeed(train);
+	local trainSpeed    = getTrainSpeed(train);
+
+	local pullingForce  = settings.global["modtrainspeeds-locomotive-pullforce"].value * getLocomotiveCount(train);
+	local totalForce    = pullingForce;
 	
-	local pullingForce = getLocomotiveCount(train) * 2500.0;
-	local totalForce = pullingForce;
-	
-	local wheelFriction = trainSpeed * 0.5 * (getLocomotiveCount(train) + getCargoWagonCount(train) + getFluidWagonCount(train));
-	local airFriction = trainSpeed * trainSpeed * 0.1;
+	local vehicleCount  = getLocomotiveCount(train) + getCargoWagonCount(train) + getFluidWagonCount(train);
+	local wheelFriction = settings.global["modtrainspeeds-train-wheelfriction-coefficient"].value * trainSpeed * vehicleCount;
+	local airFriction   = settings.global["modtrainspeeds-train-airfriction-coefficient"].value   * math_pow2(trainSpeed);
 	local totalFriction = wheelFriction + airFriction;
-	
-	if totalFriction > totalForce then
-		totalFriction = totalForce;
-	end
-	
-	return totalForce - totalFriction;
+
+	return math.max(0.0, totalForce - totalFriction);
 end
 
 
@@ -129,10 +149,12 @@ function adjustTrainAccleration(train)
 	end
 	
 	local prevSpeed = global.modtrainspeeds.trainId2speed[train.id];
-	local acceleration = (currSpeed - prevSpeed) * 60.0;
+	local acceleration = (currSpeed - prevSpeed) * GAME_FRAMERATE;
 	local didChange = 0;
 	
-	local maxAcceleration = global.modtrainspeeds.trainId2force[train.id] / global.modtrainspeeds.trainId2mass[train.id];
+	local trainForce = global.modtrainspeeds.trainId2force[train.id];
+	local trainMass  = global.modtrainspeeds.trainId2mass[train.id];
+	local maxAcceleration = trainForce / trainMass;
 	
 	if currSpeed > 0.1 and acceleration > maxAcceleration then
 		acceleration = maxAcceleration;
@@ -145,42 +167,73 @@ function adjustTrainAccleration(train)
 	
 	if didChange == 1 then
 		currSpeed = prevSpeed + acceleration;
-		train.speed = currSpeed / 60.0 / 3.6;
+		setTrainSpeed(train, currSpeed);
 	end
 	
 	global.modtrainspeeds.trainId2speed[train.id] = getTrainSpeed(train);
 end
 
+
+
+function addNiceSmokePuffsWhenDeparting(train, smokeInterval, tickCount)
+	local trainSpeed = math.abs(getTrainSpeed(train));
+	for direction, locomotives in pairs(train.locomotives) do
+		for idx, locomotive in ipairs(locomotives) do
+			locomotive.create_build_effect_smoke();
+			
+			if trainSpeed > 0.0 then
+				local modulo = 0
+				if trainSpeed < 2.5 then
+					modulo = 0.50
+				elseif trainSpeed < 5.0 then
+					modulo = 0.25
+				elseif trainSpeed < 10.0 then
+					modulo = 0.15
+				elseif trainSpeed < 25.0 then
+					modulo = 0.10
+				else
+					modulo = 0.00
+				end
+				
+				if global.modtrainspeeds.rndm() < modulo then
+					locomotive.surface.create_trivial_smoke({name='tank-smoke', position=locomotive.position})
+				end
+			end
+		end
+	end
+end
+
+
+
 script.on_event({defines.events.on_tick},
 	function (e)
 		ensure_mod_context();
 		
-		if (e.tick % 120 == 0) then
+		local discoveryInterval = 120;
+		local smokeInterval = 10;
+		local adjustInterval = 1;
+		
+		if (e.tick % discoveryInterval == 0) then
 			findTrains();
+			
 			for trainId, train in pairs(global.modtrainspeeds.trainId2train) do
 				if train.valid then
-					global.modtrainspeeds.trainId2mass[trainId] = getTrainMass(train);
+					global.modtrainspeeds.trainId2mass[trainId]  = getTrainMass(train);
 					global.modtrainspeeds.trainId2force[trainId] = getTrainForce(train);
 				end
 			end
 		end
 		
-		if (e.tick % 10 == 0) then
-			for trainId, train in pairs(global.modtrainspeeds.trainId2train) do
-				if train.valid then
-					for direction, locomotives in pairs(train.locomotives) do
-						for idx, locomotive in ipairs(locomotives) do
-							locomotive.create_build_effect_smoke();
-						end
+		for trainId, train in pairs(global.modtrainspeeds.trainId2train) do
+			if train.valid then
+				if train.state == defines.train_state.on_the_path or train.state == defines.train_state.manual_control then
+					if (e.tick % smokeInterval == trainId % smokeInterval) then
+						addNiceSmokePuffsWhenDeparting(train, smokeInterval, e.tick);
 					end
-				end
-			end
-		end
-		
-		if global.modtrainspeeds.trainId2train then
-			for trainId, train in pairs(global.modtrainspeeds.trainId2train) do
-				if train.valid then
-					adjustTrainAccleration(train);
+					
+					if (e.tick % adjustInterval == trainId % adjustInterval) then					
+						adjustTrainAccleration(train);
+					end
 				end
 			end
 		end
---
+++ TrainSpeeds/settings.lua
@@ -0,0 +1,81 @@
+data:extend
+{
+	{
+		type = "double-setting",
+		name = "modtrainspeeds-locomotive-pullforce",
+		order = "101",
+		setting_type = "runtime-global",
+		default_value = 2500,
+		minimum_value = 1000,
+		maximum_value = 10000
+	},
+	{
+		type = "double-setting",
+		name = "modtrainspeeds-locomotive-weight",
+		order = "102",
+		setting_type = "runtime-global",
+		default_value = 12500,
+		minimum_value = 1000,
+		maximum_value = 100000
+	},
+	
+	
+	{
+		type = "double-setting",
+		name = "modtrainspeeds-cargo-wagon-weight",
+		order = "201",
+		setting_type = "runtime-global",
+		default_value = 10000,
+		minimum_value = 1000,
+		maximum_value = 100000
+	},
+	{
+		type = "double-setting",
+		name = "modtrainspeeds-cargo-payload-weight",
+		order = "202",
+		setting_type = "runtime-global",
+		default_value = 10000,
+		minimum_value = 1000,
+		maximum_value = 100000
+	},
+	
+	
+	{
+		type = "double-setting",
+		name = "modtrainspeeds-fluid-wagon-weight",
+		order = "301",
+		setting_type = "runtime-global",
+		default_value = 5000,
+		minimum_value = 1000,
+		maximum_value = 100000
+	},
+	{
+		type = "double-setting",
+		name = "modtrainspeeds-fluid-payload-weight",
+		order = "302",
+		setting_type = "runtime-global",
+		default_value = 15000,
+		minimum_value = 1000,
+		maximum_value = 100000
+	},
+	
+	
+	{
+		type = "double-setting",
+		name = "modtrainspeeds-train-airfriction-coefficient",
+		order = "401",
+		setting_type = "runtime-global",
+		default_value = 0.1,
+		minimum_value = 0.0,
+		maximum_value = 1.0
+	},
+	{
+		type = "double-setting",
+		name = "modtrainspeeds-train-wheelfriction-coefficient",
+		order = "402",
+		setting_type = "runtime-global",
+		default_value = 0.5,
+		minimum_value = 0.0,
+		maximum_value = 1.0
+	}
+}
